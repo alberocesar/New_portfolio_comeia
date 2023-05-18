7edf5d66cfcfb8aebd9e1dc83434019da5b41632
alberocesar <betinho.rodolfo@gmail.com>
2023-05-17 21:05:50 -0300

    foto

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,10 +6,18 @@ import portfolio from "./Portfolio.json";
 
 function Portfolio() {
 
+    const image = [
+        {
+          "link": "https://alberocesar.github.io/Chal-_comeiaAcademy/",
+          "image": "https://cdn.pixabay.com/photo/2015/09/04/05/56/sunset-921630_1280.jpg",
+          "title": "Card Interativo"
+        }
+      ]
+
 
     return (
         <main>
-            {portfolio.map((item, index) => (
+            {image.map((item, index) => (
                 <ItemPortfolio
                     key={index}
                     link={item.link}
