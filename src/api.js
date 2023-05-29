5598381c77c3496bf92cdd4b394da15582950a85
alberocesar <betinho.rodolfo@gmail.com>
2023-05-29 17:00:33 -0300

    adicionado nova api e um novo Header

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://json-server-ex-a9rd.onrender.com'
+    baseURL: 'http://localhost:5000'
 });
+
+// https://json-server-ex-a9rd.onrender.com
 
 export default api;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./Header.css";
 
 
-function Header({nome, foto, cargo}) {
+export function Header(props) {
+    const { nome, cargo, foto } = props.informacoes;
+
     return (
         <header>
-            <img src={foto} alt="Foto de rosto de Alberto" />
+            <img src={foto} />
             <h1 id="nome-perfil">{nome}</h1>
             <h3 id="eventoSubtitulo">{cargo}</h3>
         </header>
-    )
+    );
 }
 
 export default Header;
